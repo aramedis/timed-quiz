767a19d970dc9ac0c454f48b9524c3673c231972
aramedis <119070223+aramedis@users.noreply.github.com>
2023-01-25 06:46:18 +0000

    score calculation as a % and passing of initials to console for Highscore Local Storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 //list all elements we will be monitoring
-var startBtnEl = document.querySelector(".startBtn");
+var startBtnEl = document.querySelector("#startBtn");
 var timerEl = document.querySelector(".timer");
 var qTitle = document.querySelector("#question-title");
 var qChoices = document.querySelector(".choices");
+var endScreen = document.querySelector("#end-screen");
+var finalScore = document.querySelector("#final-score");
+var submitScore = document.querySelector("#submit");
+var initialsEl = document.querySelector("#initials");
 
 
 
 //START button
 startBtnEl.addEventListener('click', function() {
+        startBtnEl.setAttribute("class", "hide");
         startTimer();
         console.log("button pressed");
         addButtons();
@@ -47,7 +52,7 @@ var questionKeys = Object.keys(question);
 var questionLength = questionKeys.length;
 function addButtons() {
     
-    //wipe childs of choices <div>
+    //wipe childs of choices <div> - need to do this to delete previous question buttons
     while (qChoices.firstChild) {
         qChoices.removeChild(qChoices.firstChild);
       }
@@ -62,9 +67,15 @@ function addButtons() {
         var button = document.createElement('button');
         button.innerHTML = questionKeys[j];
         
-        //on buttonclick record true/false value of answer in userAnswers array
+        //on buttonclick record true/false value of answer in userAnswers array and subtract 10 second time
         button.onclick = function() {
         userAnswers.push(question[this.innerHTML]);
+            if (question[this.innerHTML] === false) {
+                secondsElapsed +=10
+            };
+
+
+        //question button creation loop
         i++;
         if (i < questions.length) {
             question = questions[i][1];
@@ -74,6 +85,23 @@ function addButtons() {
         } else {
             //test console log of userAnswers
             console.log(userAnswers);
+            
+            //hide and show various elements on end of quiz
+            qChoices.setAttribute("class", "hide");
+            endScreen.setAttribute("class", "show");
+
+            //Calculate Score
+            var score = 0
+            userAnswers.forEach(element => {
+                if (element) {
+                    score++
+                }
+            });
+
+            //turn score into percentage
+            let scoreCalc = score / userAnswers.length * 100 + "%" ;
+
+            finalScore.textContent = scoreCalc
         }
         };
         qChoices.appendChild(button);
@@ -83,8 +111,31 @@ function addButtons() {
 
 
 //score screen and prompt to insert name for highscore
+initialsEl.addEventListener('input', function(){
+    initials = initialsEl.value
+})
 
-
+submitScore.addEventListener('click', writeHighScore);
+function writeHighScore() {
+    console.log("The initials for high score is "+ initials);
+    setScore();
+}
 
 
 //pass username and score to highscore local storage
+// Updates score on highscore and sets highscore to client storage
+function setScore() {
+    localStorage.setItem("initials", scoreCalc);
+  }
+
+////HIGHSCORE - The init function is called when the page loads
+function init() {
+    getScore();
+  }
+
+function getScore() {
+  // Get stored value from client storage, if it exists
+  var storedScore = localStorage.getItem("initials");
+} 
+
+init();
